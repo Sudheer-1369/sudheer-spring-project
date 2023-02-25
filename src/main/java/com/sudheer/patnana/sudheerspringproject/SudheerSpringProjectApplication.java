dc612339650a84744bf52559a796ed9fb45d03ca
sudheer patnana <sudheerpatnana45@gmail.com>
2023-02-25 15:55:14 +0530

    Initial commit

---
+++ src/main/java/com/sudheer/patnana/sudheerspringproject/SudheerSpringProjectApplication.java
@@ -0,0 +1,13 @@
+package com.sudheer.patnana.sudheerspringproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudheerSpringProjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SudheerSpringProjectApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/sudheer/patnana/sudheerspringproject/SudheerSpringProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sudheer.patnana.sudheerspringproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudheerSpringProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
